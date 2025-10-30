4f436a7196e3eff40a3626cdb1315d6bc38457f0
drakomine <prantik150personal@gmail.com>
2025-10-30 19:25:57 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const API_ENDPOINT = "http://gc1.lordcloud.ovh:25531/api/rank-purchase";
 const API_KEY = "drakosecretkey"; // Matches the Skript option
 const ADMIN_EMAIL = "drakomine.official@gmail.com";
-const qrCodeImage = "/payment-qr.jpg"; // Placeholder path
+const qrCodeImage = "IMG_20251028_214647.jpg"; // Placeholder path
 
 const ranksData = [
     {
